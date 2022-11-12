4fddd5a9f683dc4947c420e52945b3a737c3e766
Obi Kastanya <obi.kastanya@sigma-tech.co.id>
2022-11-12 22:50:24 +0700

    simple api with docs

--- models/cat.py
+++ models/cat.py
@@ -1,4 +1,4 @@
-from flask_restx import fields, reqparse
+from flask_restx import fields
 
 class CatModel:
     def __init__(self, namespace):
@@ -12,31 +12,6 @@ class CatModel:
             "message":fields.String()
         }
         return self.namespace.model('get_cats_model',data_model )
-    
-    def add_new_cat_expected_payload(self):
-        data_model={
-            "cat_name":fields.String(required=True, description="The cat name")
-        }
-        return self.namespace.model('add_cat_expected_payload',data_model )
-    
-    def get_cats_expected_params(self):
-        parser=reqparse.RequestParser()
-        parser.add_argument("cat_name", type=str, help="Optional filter by cat names", location="args" )
-        return parser
-
-    def get_cat(self):
-        data_model={
-            "data":fields.Nested(self._a_cat()),
-            "message":fields.String()
-        }
-        return self.namespace.model('get_cat',data_model )
-    
-    def add_new_cat(self):
-        data_model={
-            "data":fields.Nested(self._a_cat()),
-            "message":fields.String()
-        }
-        return self.namespace.model('add_new_cat',data_model )
 
     def _a_cat(self):
         data_model={
--- routes/cat.py
+++ routes/cat.py
@@ -1,7 +1,5 @@
-from flask import request
 from flask_restx import Namespace, Resource
 from models.cat import CatModel
-from auth import login_required
 
 api = Namespace('Cats',"APIs related to cat modules")
 cat_model=CatModel(api)
@@ -13,58 +11,9 @@ cat_dummy_data=[
 ]
 
 @api.route('/')
-class CatList(Resource):
+class CatResource(Resource):
 
-    
-    @api.doc(security="apikey")
-    @login_required
-    @api.expect(cat_model.get_cats_expected_params())
     @api.marshal_with(cat_model.get_cats())
     def get(self):
         "List all cats"
-        filter_cat_name=request.args.get("cat_name", "")
-        
-        if not filter_cat_name:
-            return {"data":cat_dummy_data, "message":"OK"}, 200
-        
-        cats=[]
-        for cat in cat_dummy_data :
-            if cat.get("name").lower() == filter_cat_name.lower():
-                cats.append(cat)
-        
-        if cats:
-            return {"data":cats, "message":"OK"}, 200
-        
-        return {"data":cats, "message":"Cats is not found"}, 404
-    
-    
-    @api.doc(security="apikey")
-    @login_required
-    @api.expect(cat_model.add_new_cat_expected_payload())
-    @api.marshal_with(cat_model.add_new_cat())
-    def post(self):
-        "Add Cat"
-        new_cat={
-            "id":int(cat_dummy_data[-1].get('id')) +1,
-            "name":request.json.get("cat_name")
-        }
-        cat_dummy_data.append(new_cat)
-
-        return {"data":new_cat, "message":"OK"}, 200
-
-
-@api.route('/<cat_id>')
-class CatList(Resource):
-
-    
-    @api.doc(security="apikey")
-    @login_required
-    @api.marshal_with(cat_model.get_cat())
-    def get(self, cat_id):
-        "Get cat"
-        for cat in cat_dummy_data:
-            if cat.get("id") == cat_id:
-                return {"data":cat, "message":"OK"}, 200
-
-        return {"data":{}, "message":"Cat is not found"}, 404
-
+        return {"data":cat_dummy_data, "message":"Cats is not found"}, 200
